66e165ef53cc46233a72fa09035f1211a24903e5
Ayush <ayushjadaun6@gmail.com>
2025-01-12 16:32:33 +0530

    made some changes

--- index.js
+++ index.js
@@ -4,59 +4,62 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs'); 
+app.set("view engine", "ejs");
 
-app.use(express.static("public")); 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
 
-const title=[]
-const bod = []
-
-app.post("/submit",(req,res)=>{
-	const data = (req.body);
-	title.push(req.body.Title)
-	bod.push(req.body.Content)
-
-	console.log(title,bod)
-	
-	 //if you have any search query write req.query
-	 res.render("main.ejs",{bod,title})
-	 
-})
-let a;
-app.get("/",(req,res)=>{
-	res.render('main.ejs')});
+const titles = [];
+const bodies = [];
 
 
-app.get("/blog",(req,res)=>{
-	const selectedTitle = req.query.title;
-	const content  = bod[title.indexOf(selectedTitle)];
-	res.render("blog.ejs",{a:selectedTitle,content});
+app.post("/submit", (req, res) => {
+  const { Title, Content } = req.body;
+  titles.push(Title);
+  bodies.push(Content);
 
-})
-app.get("/edit",(req,res)=>{
-	const Title = req.query.title;
-	const content  = bod[title.indexOf(Title)];
-	res.render("edit.ejs",{Title,content});
-	
-})
-
-
-app.post('/edit', (req, res) => {
-    // Extract the data sent from the frontend
-    const { name, email } = req.body;
-
-    // You can perform any operation with the data here
-    // For example, saving to a database (this is just a mock response)
-    
-    console.log('Received data:', { name, email });
-
-    // Respond with a success message
-    res.json({
-        message: 'Data submitted successfully',
-        data: { name, email }
-    });
+  console.log(titles, bodies);
+  res.render("main.ejs", { titles, bodies });
 });
-app.listen(port, ()=>{
-	console.log("Listening at port:3000")
-})
+
+
+app.get("/", (req, res) => {
+  res.render("main.ejs");
+});
+
+
+app.get("/blog", (req, res) => {
+  const selectedTitle = req.query.title;
+  const content = bodies[titles.indexOf(selectedTitle)];
+  res.render("blog.ejs", { title: selectedTitle, content });
+});
+
+
+app.get("/edit", (req, res) => {
+  const title = req.query.title;
+  const content = bodies[titles.indexOf(title)];
+  res.render("edit.ejs", { title, content });
+});
+
+
+app.patch("/edit/:title", (req, res) => {
+  const titleToEdit = req.params.title;
+  const newContent = req.body.content;
+
+  const index = titles.indexOf(titleToEdit);
+  if (index !== -1) {
+    bodies[index] = newContent;
+    res.json({
+      message: "Post updated successfully",
+      title: titleToEdit,
+      content: newContent,
+    });
+  } else {
+    res.status(404).json({ message: "Post not found" });
+  }
+});
+
+app.listen(port, () => {
+  console.log("Listening at port:3000");
+});
